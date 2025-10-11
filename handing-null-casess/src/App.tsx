c94e41e9db8474f84807c288bad3fdb4b7981db8
nurdawletmaksetov <nurdawletmaksetov@gmail.com>
2025-10-11 12:01:13 +0500

    hanling null cases

---
+++ handing-null-casess/src/App.tsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+
+type User = {
+  name: string,
+  age: number
+}
+
+function App() {
+  const [user, setUser] = useState<User | null>(null)
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    setUser({name: "Nurdawlet", age: 16})
+    setLoading(false)
+  }, [])
+
+  return (
+    <>
+      {!loading && user && user.name}
+      {/* {JSON.stringify(user?.name)} */}
+    </>
+  )
+}
+
+export default App
